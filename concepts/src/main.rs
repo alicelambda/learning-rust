a73f770aef5618933d760455f3cc48514eecf975
Alice Reuter <alice@alicereuter.com>
2020-11-28 12:23:53 -0600

    data types

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -1,6 +1,38 @@
 fn main() {
 
-    let mut spaces = "   ";
-    spaces = spaces.len();
-    println!(" the value of spaces is {}",spaces);
+    let guess: u32 = "42".parse().expect("not a number");
+
+    let x = 2.0;
+
+    let y: f32 = 3.0;
+
+    let t = true;
+
+    let c = 'z';
+
+    let uwu = '💜';
+
+    let tup: (i32, f64, u8) = (500,5.4,11);
+
+    let (x,y,z) = tup;
+
+
+
+    println!("{}",uwu);
+
+    println!("{}",y);
+
+
+    // select part of tuple
+    println!("{}",tup.0);
+
+    let a = [1, 2, 3, 4, 5, 6, 7];
+
+    let months = ["jan","feb","mar","apr","may","jun","aug","sep","oct","nov","dec"];
+
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("array {}",a[0]);
 }
+
